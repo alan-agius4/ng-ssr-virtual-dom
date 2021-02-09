477ec0f21d633d53418172b8284f4fd2743becca
Alan Agius <alan.agius4@gmail.com>
2021-02-09 11:42:06 +0100

    Update browser.module.ts

--- projects/ngssr/server/browser/src/lib/browser.module.ts
+++ projects/ngssr/server/browser/src/lib/browser.module.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { APP_ID, ModuleWithProviders, NgModule } from '@angular/core';
+import { DOCUMENT, ɵPLATFORM_SERVER_ID as PLATFORM_SERVER_ID } from '@angular/common';
+import { APP_ID, ModuleWithProviders, NgModule, PLATFORM_ID } from '@angular/core';
 import { ɵSharedStylesHost as SharedStylesHost, ɵDomSharedStylesHost as DomSharedStylesHost, BrowserModule } from '@angular/platform-browser';
 import { SSRStylesHost } from './styles_host';
 
@@ -16,13 +16,14 @@ export class SSRBrowserModule {
       return {
         ngModule: SSRBrowserModule,
         providers: [
+          { provide: PLATFORM_ID, useValue: PLATFORM_SERVER_ID },
           { provide: SSRStylesHost, useClass: SSRStylesHost, deps: [DOCUMENT, APP_ID] },
           { provide: SharedStylesHost, useExisting: SSRStylesHost },
           { provide: DomSharedStylesHost, useClass: SSRStylesHost },
         ]
       };
     }
-    
+
     return {
       ngModule: SSRBrowserModule
     };
